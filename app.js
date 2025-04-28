804af70ea3a2b4b8f13df30e7dc36e2f5018bea4
RealitiNova <raphhmash65@gmail.com>
2025-04-29 00:15:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Force port binding for Render and other platforms
+app.set('port', process.env.PORT || 5000);
+
+// Middleware (optional, you can add later)
+// app.use(express.json());
+
 // Basic route for testing
 app.get('/', (req, res) => {
   res.send('EduWriterPro Backend Running ✅');
 });
 
+// Export the app for server.js to use
 module.exports = app;
