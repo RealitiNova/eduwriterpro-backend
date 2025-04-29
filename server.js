e9b0f42fc5714ad85f432fb9bc8f076d6396e72e
RealitiNova <raphhmash65@gmail.com>
2025-04-29 13:07:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error('MongoDB connection error:', err));
