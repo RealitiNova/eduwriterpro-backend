16f1d5d9736c52bbdcab57a20eccb20bde032beb
RealitiNova <raphhmash65@gmail.com>
2025-04-30 16:40:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Enable CORS for your frontend domain
 app.use(cors({
-  origin: 'https://eduwriterspro.com', // Allow only your frontend
+  origin: 'https://eduwriterpro.com', // Allow only your frontend
 }));
 
 // Middleware
